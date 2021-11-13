527a3f3a405cdf7128907ff0a49a2c73c0e1f25e
Jean Pierre Acebedo <jacbedo@sfu.ca>
2021-11-13 15:30:41 -0800

    add basic note create form

--- myplanner/src/components/Notecreate.js
+++ myplanner/src/components/Notecreate.js
@@ -2,8 +2,11 @@ import React from 'react';
 import {
   Container,
   Row,
-  Col
-} from 'react-bootstrap'
+  Col,
+  Form,
+  Button
+} from 'react-bootstrap';
+
 
 function  Notecreate(){
   return (
@@ -12,7 +15,21 @@ function  Notecreate(){
         <Row md={2} id="notecreate-header">
           <h2>Create Note</h2>
         </Row>
-        
+        <Row md={10} id="notecreate-form">
+          <Form>
+            <Form.Group controlId="form-note-title" className="mb-3">
+              <Form.Label>Title</Form.Label>
+              <Form.Control type="text" placeholder="Enter note title here..."/>
+            </Form.Group>
+            <Form.Group controlId="form-note-body" className="mb-3">
+              <Form.Label>Note</Form.Label>
+              <Form.Control id="notecreate-form-body" as="textarea"/>
+            </Form.Group>
+            <Form.Group controlId="form-note-actions">
+                <Button id="form-note-submit" variant="success">Submit</Button>
+            </Form.Group>
+          </Form>
+        </Row>
       </Col>
     </Container>
   )
