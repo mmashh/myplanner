4913d25e71e1001b7c68c35bc1e04984a7071535
Jean Pierre Acebedo <jacbedo@sfu.ca>
2021-11-18 19:59:26 -0800

    remove deprecated comments

--- myplanner/client/src/utils/apiHelpers.js
+++ myplanner/client/src/utils/apiHelpers.js
@@ -5,19 +5,17 @@ const base_url = "http://localhost:5000"
 
 const getUrl = route => base_url + route;
 
-// Invokes an HTTP GET request to url; returns a promise containing the json data returned by the server.
 const httpGet = async function(url){
   var response = await axios.get(getUrl(url));
   return response.data;
 }
 
-// Invokes an HTTP GET request to url; returns a promise containing the json data returned by the server.
 const httpPost = async function(url,data){
   var response = await axios.post(getUrl(url),data)
   return response.data;
 }
 
-const httpPut = async function(url,id,data){}
+const httpPut = async function(url,id,data){/* TODO */}
 
 const httpDelete = async function(url){
   var response = await axios.delete(getUrl(url));
