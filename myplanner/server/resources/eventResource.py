aa607a4b0c86cfcdbe31ba9d09cd28ad3f477fcb
Aki Zhou <aki@akizhou.com>
2021-11-26 20:26:33 +0900

    Refactor put event

--- myplanner/server/resources/eventResource.py
+++ myplanner/server/resources/eventResource.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from abc import abstractmethod
+from typing import Type
 from flask_restful import Resource, reqparse
 from models.eventModel import EventModel
 from flasgger import swag_from
@@ -25,29 +26,35 @@ class EventAdd(Resource):
 
 class EventEdit(Resource):
     parser = reqparse.RequestParser()
-    parser.add_argument("title", type=str, required=False)
+    parser.add_argument("title", type=str, required=True)
     parser.add_argument("body", type=str, required=False)
     parser.add_argument("date", type=str, required=False)
 
-    # PUT /event/
+    # PUT /event/{event_id}
     @swag_from("../swagger_documentation/event-put.yml")
     def put(self, event_id):
         event_to_update = EventModel.find_by_id(event_id)
+        if event_to_update is None:
+            return {"message": "No event with specified id was found"}, 422
+        else:
+            new_event_attributes = self.parser.parse_args()
 
-        new_event_attributes = self.parser.parse_args()
+            event_to_update.title = new_event_attributes["title"]
+            event_to_update.body = new_event_attributes["body"]
+            try:
+                event_to_update.date = datetime.strptime(
+                    new_event_attributes["date"], r"%d/%m/%Y %H:%M"
+                )
+            except ValueError:
+                return {
+                    "message": "Incorrect datetime format. Datetime must be in 'DD/MM/YYYY hh:mm' format"
+                }, 400
+            except TypeError:
+                pass  # This should be occuring when no date was provided. If that's the case it is valid.
 
-        event_to_update.title = new_event_attributes["title"]
-        event_to_update.body = new_event_attributes["body"]
-        try:
-            event_to_update.date = datetime.strptime(
-                new_event_attributes["date"], r"%d/%m/%Y %H:%M"
-            )
-        except ValueError:
-            return {"message": "Bad date format"}, 400
+            event_to_update.save_to_db()
 
-        event_to_update.save_to_db()
-
-        return {"message": "Event successfully updated"}, 201
+            return {"message": "Event successfully updated"}, 200
 
     # DELETE /event/{event_id}
     @swag_from("../swagger_documentation/event-delete.yml")
