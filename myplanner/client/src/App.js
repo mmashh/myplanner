785703fa01078248a9cbdbb3e535cb39bd2d55c8
mmashhur <mmashhur@sfu.ca>
2021-12-05 10:51:37 +0600

    calendar mvp complete

--- myplanner/client/src/App.js
+++ myplanner/client/src/App.js
@@ -7,7 +7,8 @@ import {
   Routes,
   Link,
   Navigate,
-  useNavigate
+  useNavigate,
+  useLocation
 } from 'react-router-dom';
 import {
   Container,
@@ -19,86 +20,106 @@ import {
 } from 'react-bootstrap';
 import Cookies from 'universal-cookie';
 
-function App() {
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import EventCalendar from "./components/calendar/calendarMain";
+import { NavBar } from "./components/navBar.js";
 
+function App(props) {
   const cookies = new Cookies();
   const navigate = useNavigate();
 
-  const isLoggedIn = function() {
-    return cookies.get('Authorization') !== undefined;
-  }
+  const isLoggedIn = function () {
+    return cookies.get("Authorization") !== undefined;
+  };
 
-  const NavbarOptions = function() {
+  const NavbarOptions = function () {
+    const {pathname} = useLocation()
     if (isLoggedIn()) {
       return (
         <>
           <Nav.Item>
-            <Link to="/calendar" className="app-navbar-link">Calendar</Link>
+            <Link to="/calendar" className="app-navbar-link">
+              <Button variant={pathname == '/calendar' ? 'primary':'secondary'} >
+                Calendar
+              </Button>
+            </Link>
           </Nav.Item>
           <Nav.Item>
-            <Link to="/items" className="app-navbar-link">Items</Link>
+            <Link to="/items" className="app-navbar-link">
+            <Button variant={pathname == '/items' ? 'primary':'secondary'} >
+                Items
+              </Button>
+            </Link>
           </Nav.Item>
         </>
       );
     } else {
       return (
         <Nav.Item>
-          <Link to="/login" className="app-navbar-link">Log In</Link>
+          <Link to="/login" className="app-navbar-link">
+            Log In
+          </Link>
         </Nav.Item>
-      )
+      );
     }
-  }
+  };
 
-  const logout = function() { 
-    cookies.remove('Authorization');
-    navigate('/');
-  }
+  const logout = function () {
+    cookies.remove("Authorization");
+    navigate("/");
+  };
 
-  const redirectIfNoUser = function(element) {
-    return (!isLoggedIn()) ? <Navigate to="/login"/> :  element;
-  }
+  const redirectIfNoUser = function (element) {
+    return !isLoggedIn() ? <Navigate to="/login" /> : element;
+  };
 
-  const NavbarSignOutButton = function(){
-    if (isLoggedIn()){
+  const NavbarSignOutButton = function () {
+    if (isLoggedIn()) {
       return (
         <Nav.Item className="me-4">
-          <Button variant="secondary" onClick={logout}>Log Out</Button>
+          <Button variant="danger" onClick={logout}>
+            Log Out
+          </Button>
         </Nav.Item>
-      )
+      );
     }
-  }
+  };
 
   return (
-    <Container id="app-container">
-      <Row md={3} id="app-navbar">
-        <Col md={12}>
-          <Navbar bg="dark" variant="dark" expand="md">
-            <Navbar.Brand>
-              <span id="app-navbar-header">MyPlanner</span>
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="app-navbar-toggle" />
-            <Navbar.Collapse id="app-navbar-toggle">
-              <Nav className="me-auto">
-                {NavbarOptions()}
-              </Nav>
-              <Nav>
-                {NavbarSignOutButton()}
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-        </Col>
-      </Row>
-      <Row md={9}>
-        <Routes>
-          <Route path="/" element={isLoggedIn() ? <Navigate to="/calendar"/> : <Navigate to="/login"/>}/>
-          <Route path="/login" element={isLoggedIn() ? <Navigate to="/calendar"/> : <Login/>}/>
-          <Route path="/register" element={<Register/>}/>
-          <Route path="/items" element={redirectIfNoUser(<Items/>)}/>
-          <Route path="/calendar" element={redirectIfNoUser(<div>calendar</div>)}/>
-          <Route path="/*" element={redirectIfNoUser(<div>calendar</div>)}/>
-        </Routes>
-      </Row>
-    </Container>
+    <div className="maincontainer">
+      <Container id="app-container">
+        <Row md={3} id="app-navbar">
+          <Col md={12}>
+            <NavBar options={NavbarOptions} signout={NavbarSignOutButton}/>
+          </Col>
+        </Row>
+        <Row md={9}>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                isLoggedIn() ? (
+                  <Navigate to="/calendar" />
+                ) : (
+                  <Navigate to="/login" />
+                )
+              }
+            />
+            <Route
+              path="/login"
+              element={isLoggedIn() ? <Navigate to="/calendar" /> : <Login />}
+            />
+            <Route path="/register" element={<Register />} />
+            <Route path="/items" element={redirectIfNoUser(<Items />)} />
+            <Route
+              path="/calendar"
+              element={redirectIfNoUser(<EventCalendar />)}
+            />
+            <Route path="/*" element={redirectIfNoUser(<div>calendar</div>)} />
+          </Routes>
+        </Row>
+      </Container>
+    </div>
   );
 }
 
