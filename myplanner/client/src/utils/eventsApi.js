3ac3f28d3615c2c79bcad3c9ea3f5802ee91f155
jacebedo <jacebedo@sfu.ca>
2021-12-11 12:55:29 -0800

    Add try/catch to handle promise errors

--- myplanner/client/src/utils/eventsApi.js
+++ myplanner/client/src/utils/eventsApi.js
@@ -1,40 +1,61 @@
 import apiHelpers from "./apiHelpers";
-
+import handlePromiseError from "./handlePromiseError";
 const eventCreate = async (props) => {
-  let eventInfo = {
-    title: props.title,
-    body: props.body,
-    color: props.color,
-  };
-  const response = await apiHelpers.httpPost("/event", eventInfo);
-  return response;
+  try {
+    let eventInfo = {
+      title: props.title,
+      body: props.body,
+      color: props.color,
+    };
+    return await apiHelpers.httpPost("/event", eventInfo);
+  } catch (err) {
+    return handlePromiseError(err);
+  }
 };
 
 const unassignedLists = async () => {
-  const response = await apiHelpers.httpGet("/event/all/unassigned");
-  return response.data?.unassigned_events;
+  try {
+    const response = await apiHelpers.httpGet("/event/all/unassigned");
+    return response.data?.unassigned_events;
+  } catch (err) {
+    return handlePromiseError(err);
+  }
 };
 
 const assignedLists = async () => {
-  const response = await apiHelpers.httpGet("/event/all/assigned");
-  return response.data?.assigned_events;
+  try {
+    const response = await apiHelpers.httpGet("/event/all/assigned");
+    return response.data?.assigned_events;
+  } catch (err) {
+    return handlePromiseError(err);
+  }
 };
 
 const getUpcomingEvents = async (numWeeks) => {
-  const response = await apiHelpers.httpGet(`/event/upcoming/${numWeeks}`);
-  return (response.data) ? response.data["upcoming events"] : [];
+  try {
+    const response = await apiHelpers.httpGet(`/event/upcoming/${numWeeks}`);
+    return (response.data) ? response.data["upcoming events"] : [];
+  } catch (err) {
+    return handlePromiseError(err);
+  }
 }
 
 const eventEdit = async (props) => {
-  const response = await apiHelpers.httpPut(`/event/${props.event_id}`, {
-    ...props,
-  });
-  return response;
+  try {
+    return await apiHelpers.httpPut(`/event/${props.event_id}`, {
+      ...props,
+    });
+  } catch (err) {
+    return handlePromiseError(err);
+  }
 };
 
 const eventDelete = async (props) => {
-  var response = await apiHelpers.httpDelete(`/event/${props.event_id}`);
-  return response;
+  try {
+    return await apiHelpers.httpDelete(`/event/${props.event_id}`);
+  } catch (err) {
+    return handlePromiseError(err);
+  }
 };
 
 const eventApi = {
